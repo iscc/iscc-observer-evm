474db0779c25d9f6df08efbc6e7ef98556fee5e5
Titusz Pan <titusz.pan@gmail.com>
2022-04-18 11:46:11 +0200

    add basic observer flow

--- iscc_observer_evm/__main__.py
+++ iscc_observer_evm/__main__.py
@@ -1,10 +1,46 @@
 # -*- coding: utf-8 -*-
+import json
+import pathlib
+import sys
 import time
 
 import iscc_core as ic
+import requests
 from loguru import logger as log
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 import iscc_observer_evm as evm
+
+HERE = pathlib.Path(__file__).parent.absolute()
+
+w3 = Web3(Web3.WebsocketProvider(evm.config.web3_url))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+abi = json.load(open(HERE / "abi.json"))
+
+
+def get_head():
+    headers = {"Authorization": f"Bearer {evm.config.observer_token}"}
+    url = f"{evm.config.registry_url}/head/{evm.config.chain_id}"
+    log.debug(f"calling {url}")
+    response = requests.get(url, headers=headers, timeout=5)
+    if response.status_code == 401:
+        log.error(f"Authentication failed")
+        sys.exit(1)
+    if response.status_code == 422:
+        log.info(f"No historic events in registry for {evm.config.chain_id}")
+        return None
+    return response.json()
+
+
+def register(declaration: dict) -> dict:
+    headers = {"Authorization": f"Bearer {evm.config.observer_token}"}
+    url = f"{evm.config.registry_url}/register"
+    response = requests.post(url, json=declaration, headers=headers, timeout=4)
+    if response.status_code == 201:
+        return response.json()
+    print(response.status_code)
+    print(response.content)
 
 
 def main():
@@ -15,9 +51,44 @@ def main():
     log.info(f"registry:\t{evm.config.registry_url}")
     log.info(f"updates:\tevery {evm.config.update_interval} seconds")
 
-    while True:
-        log.info("observing ...")
+    while not w3.isConnected():
+        log.error(f"connection failed to {evm.config.web3_url}")
         time.sleep(evm.config.update_interval)
+
+    co = w3.eth.contract(evm.config.hub_contract, abi=abi)
+
+    while True:
+        time.sleep(evm.config.update_interval)
+        # check registry status:
+
+        head = get_head()
+        start_height = head["block_height"] + 1 if head else 0
+
+        if head is not None:
+            chain_block = w3.eth.getBlock(head["block_height"])
+            if head["block_hash"] != chain_block.hash.hex():
+                log.warning(f"registry out of sync at {head}")
+
+        # TODO rollback registry
+
+        event_filter = co.events.IsccDeclaration.createFilter(fromBlock=start_height)
+        for event in event_filter.get_all_entries():
+            block = w3.eth.getBlock(event.blockNumber)
+            declaration = dict(
+                timestamp=block.timestamp,
+                chain_id=evm.config.chain_id,
+                block_height=block.number,
+                block_hash=block.hash.hex(),
+                tx_idx=event.transactionIndex,
+                tx_hash=event.transactionHash.hex(),
+                declarer=event.args.declarer,
+                iscc_code=event.args.iscc,
+                message=event.args.message,
+                meta_url=event.args.url,
+                registrar=event.args.registrar,
+            )
+            resp = register(declaration)
+            log.info(f"registered {resp['iscc_id']} with did {resp['did']}")
 
 
 if __name__ == "__main__":
